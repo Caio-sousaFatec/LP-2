be7d37385bbcd93392a25df4c8fe56737fec94c5
Caio M Sousa <caio.sousa5@fatec.sp.gov.br>
2024-10-10 22:55:21 -0300

    refactor: refactored the project entirely, aiming for better readability and understanding

--- src/main/java/com/lp2/leilao/repository/DispositivoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lp2.leilao.repository;
-
-import com.lp2.leilao.model.Dispositivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
-}
--- src/main/java/com/lp2/leilao/repository/LoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lp2.leilao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.lp2.leilao.model.Lote;
-
-@Repository
-public interface LoteRepository extends JpaRepository<Lote, Long> {
-}
